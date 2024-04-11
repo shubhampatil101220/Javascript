45c4886fbf457c8e53629baab968641e9ec1fb35
Shubham Patil <152961327+shubhampatil101220@users.noreply.github.com>
2024-04-11 11:56:11 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+// In javascipt there are 6-7 dataTypes they are as follows
+let salary = 132000;
+let Ename ="shubham";
+let age_18 =true;
+let bank ;
+let balance =null;
+
+
+//primitive datatypes
+/*
+Number = ranges between 0 to 2 power of 53
+BigInt = whem you want to deal with bigger number
+String = "" it store the string datatypes value
+Boolean = it stores only true and false
+null = it is a standalone value its datatype name and its value both are null 
+undefined = a variable which not defined
+Symbol = unique charachters are covered under symbol datatypes 
+
+Object 
+*/
+console.log( typeof salary);
+console.log(typeof Ename);
+console.log(typeof age_18);
+console.log(typeof bank);
+console.log(typeof null);
+ //its a bug in a javascript that shows typeof null value as a object
+ //it should be null
