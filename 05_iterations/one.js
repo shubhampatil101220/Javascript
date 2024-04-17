31a0bd4a7f80258167c6a7cc86eef706e3902c69
Shubham Patil <152961327+shubhampatil101220@users.noreply.github.com>
2024-04-17 11:29:22 +0000

    iterations-2

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -1,3 +1,4 @@
+//for loop
 for (let i = 1; i <= 10; i++) {
   console.log(i);
 }
---
+++ 05_iterations/two.js
@@ -0,0 +1,19 @@
+//while loop
+let index =1
+while (index< 5) {
+    console.log(index);
+    index++;
+}
+let arr=0
+let myArray =["chai","piyo","biscuit","khawo"];
+
+while (arr <myArray.length ) {
+    console.log(myArray[arr]);
+    arr++
+}
+
+let score=191;
+do {
+    console.log(`score is defendable ${score}`);
+    score++;
+} while (score<=190);
